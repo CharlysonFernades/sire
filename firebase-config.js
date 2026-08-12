1844367e3694146eeb17a25f7078d68f8f6ccbf8
CharlysonFernades <pontescharlyson@gmail.com>
2026-08-12 16:37:10 -0300

    Primeira versão pública do SIRE

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Este arquivo fica público no GitHub Pages. Isso é normal em apps Web Firebase.
+// NÃO coloque senha, chave de Service Account ou credencial administrativa aqui.
+window.SIRE_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyC1e34DBwd6qIdEWBXc_4AYoc7RlLO0XJw",
+  authDomain: "sire-69957.firebaseapp.com",
+  projectId: "sire-69957",
+  storageBucket: "sire-69957.firebasestorage.app",
+  messagingSenderId: "737004836161",
+  appId: "1:737004836161:web:969fdcd4e0ab7463b8bdc0",
+  measurementId: "G-0NP4DXF2ZY"
+};
